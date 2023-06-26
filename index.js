b4208c097bddeff7e8da9784bfccaac144be68c3
kushal goel <kgoel8451@gmail.com>
2023-06-26 22:13:06 +0530

    updated files

--- index.js
+++ index.js
@@ -13,12 +13,33 @@ hide.addEventListener("click", function () {
   d[0].style.overflow = "hidden";
   hide.style.display = "none";
   read.style.display = "block";
+  
 });
 
 let quiz = document.getElementsByClassName("quiz");
 let showquiz = document.getElementById("test-btn");
 showquiz.addEventListener("click", function () {
   quiz[0].style.display = "block";
+  let time1;
+  let timer1=document.getElementById("text4");
+(function(){
+var sec2=60;
+time1 = setInterval(()=>{
+    timer1.innerHTML='00:'+sec2 ;
+    sec2--;
+    if(sec2==0){
+        sec2=60;
+    }
+},1000)
+
+if(sec2==0){
+    clearInterval();
+    let scores =document.getElementById("result");
+    scores.style.display="block";
+    scores.innerHTML="Your Score is "+score+" out of 4.";
+
+}
+})()
 });
 
 let score=0;
@@ -64,8 +85,21 @@ submit.addEventListener("click",function(){
     let scores =document.getElementById("result");
     scores.style.display="block";
     scores.innerHTML="Your Score is "+score+" out of 4.";
+
     
 });
+let time;
+let timer=document.getElementById("text3");
+(function(){
+var sec=60;
+time = setInterval(()=>{
+    timer.innerHTML='00:'+sec ;
+    sec--;
+    if(sec==0){
+        sec=60;
+    }
+},1000)
+})()
 
 
 
